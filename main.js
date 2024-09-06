18d075946d364588a24972337b22d28ab779fafd
stepchichi <mishrayushrm05@gmail.com>
2024-09-06 22:04:15 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+import fs from "fs/promises"
+import fsn from "fs"
+import path from "path"
+
+const basepath = "C:\\Codingdongding\\web dev\\Projects\\clutter manager"
+
+let files = await fs.readdir(basepath)
+
+for (const item of files) {
+    console.log("running for ", item)
+    let ext = item.split(".")[item.split(".").length - 1]
+    if (ext != "js" && ext != "json" && item.split(".").length > 1) {
+
+
+        if (fsn.existsSync(path.join(basepath, ext))) {
+            // Move the file to this directory if its not a js or json file
+            fs.rename(path.join(basepath, item), path.join(basepath, ext, item))
+        }
+        else {
+            fs.mkdir(ext)
+            fs.rename(path.join(basepath, item), path.join(basepath, ext, item))
+            
+        }
+    }
+
+}
